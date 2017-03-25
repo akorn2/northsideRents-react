fbe89c3b6fd341845370f4025873b72106e2b827
akorn2 <schleichkorn@gmail.com>
2017-03-24 21:25:20 -0500

    <ContactButtons />
    Colors for branding

--- src/components/ContactButtons/ContactButtons.js
+++ src/components/ContactButtons/ContactButtons.js
@@ -13,10 +13,10 @@ class ContactButtons extends React.Component {
         <ListGroupItem href="#" header="Facebook" bsStyle="info" />
         <ListGroupItem href="#" header="Yelp" bsStyle="info" />
         <ListGroupItem href="#" header="Contact Us" className={s.buttonHeader} />
-        <ListGroupItem href="#" header="Phone" bsStyle="success"> <Glyphicon glyph="glyphicon glyphicon-earphone" /> 773 474 3137</ListGroupItem>
-        <ListGroupItem href="#" header="Fax" bsStyle="success"> <Glyphicon glyph="glyphicon glyphicon-print" /> 773 474 3137</ListGroupItem>
-        <ListGroupItem href="#" header="Email" bsStyle="success"> <Glyphicon glyph="glyphicon glyphicon-envelope" /> northsideRents@gmail.com</ListGroupItem>
-        <ListGroupItem href="#" header="Mail" bsStyle="success"> <Glyphicon glyph="glyphicon glyphicon-home" /> 4556 N Street, Chicago Il 60625</ListGroupItem>
+        <ListGroupItem href="#" header="Phone" bsStyle="info"> <Glyphicon glyph="glyphicon glyphicon-earphone" /> 773 474 3137</ListGroupItem>
+        <ListGroupItem href="#" header="Fax" bsStyle="info"> <Glyphicon glyph="glyphicon glyphicon-print" /> 773 474 3137</ListGroupItem>
+        <ListGroupItem href="#" header="Email" bsStyle="info"> <Glyphicon glyph="glyphicon glyphicon-envelope" /> northsideRents@gmail.com</ListGroupItem>
+        <ListGroupItem href="#" header="Mail" bsStyle="info"> <Glyphicon glyph="glyphicon glyphicon-home" /> 4556 N Street, Chicago Il 60625</ListGroupItem>
         {credentials}
       </ListGroup>
     );
