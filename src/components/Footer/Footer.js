d988cddc8da301f71db208f38eb75d68b6667666
akorn2 <schleichkorn@gmail.com>
2017-03-24 21:24:51 -0500

    <Footer />

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,6 +8,7 @@
  */
 
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Footer.css';
 import Link from '../Link';
@@ -15,19 +16,17 @@ import Link from '../Link';
 class Footer extends React.Component {
   render() {
     return (
-      <div className={s.root}>
-        <div className={s.container}>
-          <span className={s.text}>© Your Company</span>
+      <Row className={s.root}>
+        <Col className={s.container}>
+          <span className={s.text} to="/">© Chicago Northside Reality</span>
           <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/">Home</Link>
+          <Link className={s.link} to="/admin">Contact</Link>
           <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/admin">Admin</Link>
+          <Link className={s.link} to="/privacy">Search Property</Link>
           <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/privacy">Privacy</Link>
-          <span className={s.spacer}>·</span>
-          <Link className={s.link} to="/not-found">Not Found</Link>
-        </div>
-      </div>
+          <Link className={s.link} to="/not-found">Search Rental</Link>
+        </Col>
+      </Row>
     );
   }
 }
