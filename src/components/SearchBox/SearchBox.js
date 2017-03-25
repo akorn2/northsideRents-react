1fc82795b6fe16c2844d68eabfc5cc8d4bfd088d
akorn2 <schleichkorn@gmail.com>
2017-03-24 21:41:10 -0500

    Formatted for Desktop, 1 Button & 1 text

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 import s from './SearchBox.css';
 
-const HeaderText = (
-  <div className={s.searchBox} >
-    <a>Search</a>
-  </div>
-);
-  /* <small>
-     Rent or Buy</small> */
+class SearchBox extends React.Component {
+  render() {
+    return (
+      <div className={s.searchBox} >
+        <Button bsSize="small">View Available Listings</Button>
+        <h5>View Map</h5>
+      </div>
+    );
+  }
+}
 
-export default withStyles(s)(HeaderText);
+export default withStyles(s)(SearchBox);
