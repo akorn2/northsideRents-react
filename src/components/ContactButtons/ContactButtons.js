09c44591078b787e25559790d01d43576c85f347
akorn2 <schleichkorn@gmail.com>
2017-03-24 21:01:34 -0500

    <Contact Buttons />

--- src/components/ContactButtons/ContactButtons.js
+++ src/components/ContactButtons/ContactButtons.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import { ListGroup, ListGroupItem, Glyphicon } from 'react-bootstrap';
+import s from './ContactButtons.css';
 import credentials from '../Credentials/Credentials';
 
-const ContactButtons = (
-  <ListGroup>
-    <ListGroupItem href="#" header="Share Our Services" active />
-    <ListGroupItem href="#" header="Facebook" bsStyle="info" />
-    <ListGroupItem href="#" header="Yelp" bsStyle="info" />
-    <ListGroupItem href="#" header="Contact Us" active />
-    <ListGroupItem href="#" header="Phone" bsStyle="success"> <Glyphicon glyph="glyphicon glyphicon-earphone" /> 773 474 3137</ListGroupItem>
-    <ListGroupItem href="#" header="Fax" bsStyle="success"> <Glyphicon glyph="glyphicon glyphicon-print" /> 773 474 3137</ListGroupItem>
-    <ListGroupItem href="#" header="Email" bsStyle="success"> <Glyphicon glyph="glyphicon glyphicon-envelope" /> northsideRents@gmail.com</ListGroupItem>
-    <ListGroupItem href="#" header="Mail" bsStyle="success"> <Glyphicon glyph="glyphicon glyphicon-home" /> 4556 N Street, Chicago Il 60625</ListGroupItem>
-    {credentials}
-  </ListGroup>
-);
+class ContactButtons extends React.Component {
 
-export default ContactButtons;
+  render() {
+    return (
+      <ListGroup>
+        <ListGroupItem href="#" header="Share Our Services" className={s.buttonHeader} />
+        <ListGroupItem href="#" header="Facebook" bsStyle="info" />
+        <ListGroupItem href="#" header="Yelp" bsStyle="info" />
+        <ListGroupItem href="#" header="Contact Us" className={s.buttonHeader} />
+        <ListGroupItem href="#" header="Phone" bsStyle="success"> <Glyphicon glyph="glyphicon glyphicon-earphone" /> 773 474 3137</ListGroupItem>
+        <ListGroupItem href="#" header="Fax" bsStyle="success"> <Glyphicon glyph="glyphicon glyphicon-print" /> 773 474 3137</ListGroupItem>
+        <ListGroupItem href="#" header="Email" bsStyle="success"> <Glyphicon glyph="glyphicon glyphicon-envelope" /> northsideRents@gmail.com</ListGroupItem>
+        <ListGroupItem href="#" header="Mail" bsStyle="success"> <Glyphicon glyph="glyphicon glyphicon-home" /> 4556 N Street, Chicago Il 60625</ListGroupItem>
+        {credentials}
+      </ListGroup>
+    );
+  }
+}
+
+export default withStyles(s)(ContactButtons);
