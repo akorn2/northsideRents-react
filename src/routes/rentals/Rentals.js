928a9fd1abb0631a1cb0d3f5828ade0ffe15b938
akorn2 <schleichkorn@gmail.com>
2017-03-26 14:34:43 -0500

    <Rentals />

---
+++ src/routes/rentals/Rentals.js
@@ -0,0 +1,45 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React, { PropTypes } from 'react';
+import { Col, Row, PageHeader } from 'react-bootstrap';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import SearchBox from '../../components/SearchBox/SearchBox';
+import Slide from '../../components/Slide/Slide';
+import BodyText from '../../components/Body/BodyRentals';
+import ContactButtons from '../../components/ContactButtons/ContactButtons';
+import s from './Rentals.css';
+
+class Rentals extends React.Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+  };
+
+  render() {
+    return (
+      <Row className={s.root}>
+        <Col xs={12} className={s.carousel} > { Slide } </Col>
+        <Col xs={12} className={s.header}>
+          <PageHeader>Chicago Northside Reality</PageHeader>
+          <SearchBox />
+        </Col>
+        <Col xs={12} sm={8} className={s.body}> { BodyText } </Col>
+        <Col xs={12} sm={4} className={s.sidebar}>
+          <Row>
+            <Col xs={12} className={s.contactButton}>
+              <ContactButtons />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    );
+  }
+}
+
+export default withStyles(s)(Rentals);
