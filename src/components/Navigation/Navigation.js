d3f3ae2a523d2a922c51cefd3e4c40eb4252938e
akorn2 <schleichkorn@gmail.com>
2017-03-26 14:35:07 -0500

    <Navigation />

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,7 +8,6 @@
  */
 
 import React from 'react';
-import cx from 'classnames';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Navigation.css';
 import Link from '../Link';
@@ -18,10 +17,10 @@ class Navigation extends React.Component {
     return (
       <div className={s.root} role="navigation">
         <Link className={s.link} to="/about">About</Link>
-        <Link className={cx(s.link, s.highlight)} to="/contact">Contact</Link>
+        <Link className={s.link} to="/contact">Contact</Link>
         <span className={s.spacer}> | </span>
-        <Link className={s.link} to="/contact">Find an Apartment</Link>
-        <Link className={s.link} to="/contact">Find a Property</Link>
+        <Link className={s.link} to="/rentals">Find an Apartment</Link>
+        <Link className={s.link} to="/sales">Find a Property</Link>
       </div>
     );
   }
