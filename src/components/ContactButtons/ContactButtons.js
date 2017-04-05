4d0844f260eccfd6330508e07b2603fe3c541a0e
akorn2 <schleichkorn@gmail.com>
2017-04-05 14:46:55 -0500

    color branding = dark blue

--- src/components/ContactButtons/ContactButtons.js
+++ src/components/ContactButtons/ContactButtons.js
@@ -10,13 +10,13 @@ class ContactButtons extends React.Component {
     return (
       <ListGroup>
         <ListGroupItem href="#" header="Share Our Services" className={s.buttonHeader} />
-        <ListGroupItem href="#" header="Facebook" bsStyle="info" />
-        <ListGroupItem href="#" header="Yelp" bsStyle="info" />
+        <ListGroupItem href="#" header="Facebook" bsStyle="info" className={s.buttons} />
+        <ListGroupItem href="#" header="Yelp" bsStyle="info" className={s.buttons} />
         <ListGroupItem href="#" header="Contact Us" className={s.buttonHeader} />
-        <ListGroupItem href="#" header="Phone" bsStyle="info"> <Glyphicon glyph="glyphicon glyphicon-earphone" /> 773 474 3137</ListGroupItem>
-        <ListGroupItem href="#" header="Fax" bsStyle="info"> <Glyphicon glyph="glyphicon glyphicon-print" /> 773 474 3137</ListGroupItem>
-        <ListGroupItem href="#" header="Email" bsStyle="info"> <Glyphicon glyph="glyphicon glyphicon-envelope" /> northsideRents@gmail.com</ListGroupItem>
-        <ListGroupItem href="#" header="Mail" bsStyle="info"> <Glyphicon glyph="glyphicon glyphicon-home" /> 4556 N Street, Chicago Il 60625</ListGroupItem>
+        <ListGroupItem href="#" header="Phone" bsStyle="info" className={s.buttons} > <Glyphicon glyph="glyphicon glyphicon-earphone" /> 773 474 3137</ListGroupItem>
+        <ListGroupItem href="#" header="Fax" bsStyle="info" className={s.buttons} > <Glyphicon glyph="glyphicon glyphicon-print" /> 773 474 3137</ListGroupItem>
+        <ListGroupItem href="#" header="Email" bsStyle="info" className={s.buttons} > <Glyphicon glyph="glyphicon glyphicon-envelope" /> northsideRents@gmail.com</ListGroupItem>
+        <ListGroupItem href="#" header="Mail" bsStyle="info" className={s.buttons} > <Glyphicon glyph="glyphicon glyphicon-home" /> 4556 N Street, Chicago Il 60625</ListGroupItem>
         {credentials}
       </ListGroup>
     );
