85662853f8b79ff5338e090d46cf9cf2400dcae5
akorn2 <schleichkorn@gmail.com>
2017-04-06 16:51:58 -0500

    new temporary copy

--- src/components/Body/BodyHome.js
+++ src/components/Body/BodyHome.js
@@ -1,41 +1,18 @@
 import React from 'react';
 
-const BodyText = (
+export default (
   <div>
+    <h3>Find Your Next Apartment</h3>
 
-    <h3>Boutique Management Offers Personalized Service</h3>
+    <p><bold>Chicago Northside Realty LLC</bold> represents hundreds of units.
+    Share your search criteria to find your next apartment</p>
 
-    <p><bold>Chicago Northside Realty LLC</bold> is a boutique real estate and management
-    company servicing the North Side of Chicago. We specialize in renting,
-    buying and selling North Side properties for families and investors.</p>
+    <h3>Fill your Property Vacancies</h3>
 
-    <p>Our management team offers excellent service managing a single condo, house or
-    2-6 flat. Our management partner, Property Hill Management, offers full
-    service management for larger multi-unit condo associations.</p>
+    <p>Get your vacancies filled with vetted renters fast!</p>
 
-    <h3>Rent or Buy</h3>
-
-    <p>Chicago Northside Realty LLC manages many of the buildings you see featured on our website.
-      We keep them clean so you can take pride in your home and enjoy a healthy lifestyle.
-      We have helped thousands of renters find their perfect apartment.
-      Studios, one bedrooms, two bedrooms, 3, 4, even 5 bedroom apartments and houses.</p>
-
-    <ul>
-      <li>search MLS for Rental apartments</li>
-      <li>search MLS For Sale listings</li>
-      <li>search available rentals</li>
-    </ul>
-
-    <h3>Property Management</h3>
-    <p>Our property management team will take care of your investment property.
-      With regular visits to your property, we maintain the common areas keeping
-      them clean and safe of debris.
-      We can arrange cleaning services, lawn maintenance, repairs and maintenance,
-      lock out service, rent collecting and more.
-      Everything and anything that needs to be done to maintain your property,
-      keeping it safe, clean and profitable.</p>
+    <h3>Buy Investment Properties</h3>
+    <p>Get your money to work for you with units to rent.</p>
 
   </div>
 );
-
-export default BodyText;
